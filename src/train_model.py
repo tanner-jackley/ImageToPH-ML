55f940afdffd01ccff9c071c608988d2d0076b8b
tannerjackley <s561664@nwmissouri.edu>
2026-03-27 09:58:35 -0500

    imported training images, not lining up

--- src/train_model.py
+++ src/train_model.py
@@ -1,14 +1,33 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from image_preprocessing import preprocess_image
+from pathlib import Path
 
 def train():
+    '''
     # Load data
     df = pd.read_csv("data/synthetic/ph_synthetic.csv")
 
     X = df[["Hue", "Saturation", "Value"]]
     y = df["pH"]
+    '''
 
+    folder_path = Path("data/training_images")
+    df = []
+    for image_path in folder_path.glob("*.png"):
+        hsv_from_image = preprocess_image(image_path)
+        row = pd.DataFrame(
+            [[*hsv_from_image, image_path.stem]],
+            columns=["Hue", "Saturation", "Value", "pH"]
+        )
+        df.append(row)
+
+    df = pd.concat(df, ignore_index=True)
+
+    X = df[["Hue", "Saturation", "Value"]]
+    y = df["pH"]
+    
     # Split
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
